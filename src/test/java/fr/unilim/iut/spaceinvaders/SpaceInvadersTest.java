bc9916b6e7016504f8d63209c7f9f1788308f231
Jules Artaud <artaud.jules07@gmail.com>
2021-04-28 09:52:20 +0200

    positionnerUnNouveauVaisseau

--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -1,24 +1,86 @@
 package fr.unilim.iut.spaceinvaders;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+    import static org.junit.Assert.assertEquals;
+    import org.junit.Test;
+    import static org.junit.Assert.fail;
+    import org.junit.Before;
 
-public class SpaceInvadersTest {
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 
-   @Test
-   public void test_AuDebut_JeuSpaceInvaderEstVide() {
-	    SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
-	    assertEquals("" + 
-	    "...............\n" + 
-	    "...............\n" +
-	    "...............\n" + 
-	    "...............\n" + 
-	    "...............\n" + 
-	    "...............\n" + 
-	    "...............\n" + 
-	    "...............\n" + 
-	    "...............\n" + 
-	    "...............\n" , spaceinvaders.toString());
-        }
+    public class SpaceInvadersTest {
+	
+       private SpaceInvaders spaceinvaders;
 
-   }
+	   @Before
+	   public void initialisation() {
+		  
+	   }
+	    
+	   @Test
+	   public void test_AuDebut_JeuSpaceInvaderEstVide() {
+		    SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+		    assertEquals("" + 
+		    "...............\n" + 
+		    "...............\n" +
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+	   }
+	   
+	   @Test
+		public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+			SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+			spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+			assertEquals("" + 
+			"...............\n" + 
+			"...............\n" +
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			".......V.......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+		}
+	   
+	   @Test
+		public void test_UnNouveauVaisseauPositionneHorsEspaceJeu_DoitLeverUneException() {
+			SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+			
+			try {
+				spaceinvaders.positionnerUnNouveauVaisseau(15,9);
+				fail("Position trop à droite : devrait déclencher une exception HorsEspaceJeuException");
+			} catch (final HorsEspaceJeuException e) {
+			}
+			
+			
+			try {
+				spaceinvaders.positionnerUnNouveauVaisseau(-1,9);
+				fail("Position trop à gauche : devrait déclencher une exception HorsEspaceJeuException");
+			} catch (final HorsEspaceJeuException e) {
+			}
+			
+			
+			try {
+				spaceinvaders.positionnerUnNouveauVaisseau(14,10);
+				fail("Position trop en bas : devrait déclencher une exception HorsEspaceJeuException");
+			} catch (final HorsEspaceJeuException e) {
+			}
+			
+			
+			try {
+				spaceinvaders.positionnerUnNouveauVaisseau(14,-1);
+				fail("Position trop à haut : devrait déclencher une exception HorsEspaceJeuException");
+			} catch (final HorsEspaceJeuException e) {
+			}
+				
+		}
+	   
+
+     }
